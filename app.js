1f0c031a81f29f4b03f87ba9ec9af792c5da9c0c
sagurs999 <sagurs999@gmail.com>
2026-09-09 02:21:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const videos = [
   },
   {
     id: "37urf9qdgfkf",
-    video url: "https://luluvdo.com/37urf9qdgfkf",
+    url: "https://luluvdo.com/37urf9qdgfkf",
     title: "Waitress_(2026)_Moodx_Hindi Uncut_Hot_Short Film_Watch_Free",
     category: "Popular",
     thumbnail: "https://i.ibb.co/xKHdTDx7/Waitress-2026-Moodx-Hindi-Uncut-Hot-Short-Film-Watch-Free.webp"
