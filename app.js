94073df539da0bf329b013e0ab50adb1baad3dbf
sagurs999 <sagurs999@gmail.com>
2026-09-08 19:40:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const videos = [
    {
     id: "6clvappg3m8z",
     title: "Hot Video",
-    category: "trending",
+    category: "Trending",
     thumbnail: "https://i.ibb.co/Yw7S09v/Screenshot-2026-09-08-19-34-18-93-99c04817c0de5652397fc8b56c3b3817.jpg"
   },
 ];
