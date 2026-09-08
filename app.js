ed7259089a9b823275ab619888f18e30688aaa20
sagurs999 <sagurs999@gmail.com>
2026-09-08 19:41:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const videos = [
     category: "Trending",
     thumbnail: "https://i.ibb.co/Yw7S09v/Screenshot-2026-09-08-19-34-18-93-99c04817c0de5652397fc8b56c3b3817.jpg"
   },
+   
 ];
 
 let selectedVideo = null;
