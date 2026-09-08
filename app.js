84486818eebb821e1d8a442edcdbe95ca6f9bafd
sagurs999 <sagurs999@gmail.com>
2026-09-08 19:25:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ const videos = [
    {
     id: "1zngyas64349",
     title: "Vairal Video",
-    category: "Hot Video",
+    category: "Hot video",
     thumbnail: "https://i.ibb.co/23nw3rN3/Screenshot-2026-09-08-19-13-02-92-99c04817c0de5652397fc8b56c3b3817.jpg"
-  }
+  },
 ];
 
 let selectedVideo = null;
